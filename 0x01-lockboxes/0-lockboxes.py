7124e3d782bae18393935ee7b417b6d6f4314419
Mai614 <maymohamed614@gmail.com>
2024-03-14 17:21:26 +0000

    fix errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle """
+'''A module for lockboxes.
+'''
 
-def find_next_box(boxes, opened):
-    """Find the next box to open"""
-    for index, box in enumerate(boxes):
-        if box and index not in opened:
-            return index
-    return None
 
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened"""
-    opened_boxes = set()
-    opened_boxes.add(0)
-    while True:
-        next_box = find_next_box(boxes, opened_boxes)
-        if next_box is None:
-            return len(opened_boxes) == len(boxes)
-        opened_boxes.add(next_box)
-        for key in boxes[next_box]:
-            if key < len(boxes):
-                opened_boxes.add(key)
-
-if __name__ == '__main__':
-    main()
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    num = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        box_id = unseen_boxes.pop()
+        if not box_id or box_id >= num or box_id < 0:
+            continue
+        if box_id not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box_id])
+            seen_boxes.add(box_id)
+    return num == len(seen_boxes)
