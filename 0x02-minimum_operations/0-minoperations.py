fdefba976b23c751255d1b75494bac13d1a48162
Mai614 <maymohamed614@gmail.com>
2024-03-21 15:53:41 +0000

    fixmain

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,26 @@
-def minOperations(n):
+#!/usr/bin/python3
+""" Minimum Operations
+    """
+
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
     if n <= 1:
-        return n
+        return n  # If n is 0 or 1, no operations needed
 
-    oper = 0
-    remaining = n
-    clip = 1  # Initial H character
+    op = 0
+    clipboard = 'H'
+    body = 'H'
 
-    while remaining > 1:
-        if remaining % clip == 0:
-            oper += 1
-            remaining //= clip  # Update remaining count
+    while len(body) < n:
+        if n % len(body) == 0:
+            op += 2  # Copy All and Paste
+            clipboard = body  # Update clipboard to current body
+            body += clipboard  # Paste the current body
         else:
-            # If not, copy all and paste
-            oper += 2
-            clip = remaining
+            op += 1  # Only Paste
+            body += clipboard  # Paste the clipboard
 
-        remaining -= clip
-
-    oper += remaining
-    return oper
+    if len(body) != n:
+        return 0  # Impossible to achieve n H characters
+    return op
